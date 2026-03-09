97450d90150b54915dbd51f1a77c35c5e83382d6
nyakuoff <bertrandbridou1994@gmail.com>
2026-03-09 10:11:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+const { app, BrowserWindow, shell, Menu } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    title: "Photopea",
+    autoHideMenuBar: true,
+    show: false,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  win.maximize();
+  win.show();
+
+  Menu.setApplicationMenu(null);
+
+  win.loadURL("https://www.photopea.com/");
+
+  win.webContents.on("dom-ready", () => {
+    win.webContents.executeJavaScript(`
+      Object.defineProperty(window, 'innerWidth', {
+        get: () => document.documentElement.clientWidth + 300
+      });
+      window.dispatchEvent(new Event('resize'));
+    `);
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (!url.startsWith("https://www.photopea.com")) {
+      shell.openExternal(url);
+      return { action: "deny" };
+    }
+    return { action: "allow" };
+  });
+
+  win.on("page-title-updated", (e) => {
+    e.preventDefault();
+    win.setTitle("Photopea");
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
